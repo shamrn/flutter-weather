505826688d1c48248af014e5fb7abbf04fd217e3
Vladislav Shamrin <shamrin2007@mail.ru>
2023-04-11 10:41:57 +0300

    Created common app constants

---
+++ lib/common/app_constants.dart
@@ -0,0 +1,15 @@
+// The file contains the common constants for the application
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color canvasColor = Color(0xFFF9F5EA);
+  static const Color primaryColor = Color(0xFF4169E1);
+  static const Color darkenedBackgroundColor = Color(0xFFF0ECE2);
+  static const Color primaryTextColor = Color(0xFF2D2D2B);
+}
+
+class AppStyles {
+  static const EdgeInsets primaryPadding =
+      EdgeInsets.symmetric(horizontal: 40, vertical: 20);
+}
