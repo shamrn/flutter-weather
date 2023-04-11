5af3d1cc22fa9d069edf5cffabbd2686a0721f76
Vladislav Shamrin <shamrin2007@mail.ru>
2023-04-11 12:36:38 +0300

    Impelemented detail weather screen

--- lib/common/app_constants.dart
+++ lib/common/app_constants.dart
@@ -9,6 +9,7 @@ class AppColors {
   static const Color primaryColor = Color(0xFF4169E1);
   static const Color darkenedBackgroundColor = Color(0xFFF0ECE2);
   static const Color primaryTextColor = Color(0xFF2D2D2B);
+  static const Color secondTextColor = Color(0xFF6F6F6F);
 }
 
 class AppStyles {
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_weather/common/app_constants.dart';
 import 'package:flutter_weather/common/utils.dart';
 import 'package:flutter_weather/features/presentation/screens/city_input_screen.dart';
+import 'package:flutter_weather/features/presentation/screens/detail_screen.dart';
 
 void main() {
   runApp(const WeatherApp());
@@ -20,6 +21,7 @@ class WeatherApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const CityInputScreen(),
+        '/detail': (context) => const DetailScreen(),
       },
     );
   }
